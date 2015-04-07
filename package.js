2cd53e2b479a6765134921ce808d8d21662a2b9a
Ivan Suslov <zuzel_tv@mail.ru>
2015-04-08 04:31:14 +0600

    Initial commit

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+
+	name: 'zuzel:atlassian-crowd',
+	version: '0.0.1',
+	summary: 'A Meteor package for Atlassian Crowd.',
+	documentation: 'README.md'
+
+});
+
+Package.onUse(function(api) {
+
+	api.versionsFrom('1.0');
+    api.use(['accounts-base', 'accounts-password'], 'server');
+	api.add_files( ['client/crowd_client.js'], 'client' );
+	api.add_files( ['server/crowd_server.js'], 'server' );
+
+	api.export('AtlassianCrowd', 'server');
+    api.export('ATLASSIAN_CROWD_CONFIG', 'server');
+
+});
+
+Npm.depends({ 'atlassian-crowd': 'https://github.com/dsn/node-atlassian-crowd/tarball/b9014d2266c21e9949ce9c8e7dcfae7e7a1ae501' });
+
+
